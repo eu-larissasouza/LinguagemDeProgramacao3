5b9583c408fd97ddd094fa34cfbcc9a4f2efcefc
Larissa Souza <larissa2004.souza@gmail.com>
2021-07-30 21:39:37 -0300

    Atividade - Aula 7

---
+++ AtividadesSemanais/Aula 7/Pessoa.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_7
+{
+    class Pessoa{
+
+        private string nome = "Larissa";
+        private string sobrenome = "Souza";
+        private byte idade = 17;
+        private float altura = 1.56F;
+        private float peso = 55.5F;
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getSobrenome()
+        {
+            return this.sobrenome;
+        }
+        public void setSobrenome(string sobrenome)
+        {
+            this.sobrenome = sobrenome;
+        }
+
+        public byte getIdade()
+        {
+            return this.idade;
+        }
+        public void setIdade(byte idade)
+        {
+            this.idade = idade;
+        }
+
+        public float getAltura()
+        {
+            return this.altura;
+        }
+        public void setAltura(float altura)
+        {
+            this.altura = altura;
+        }
+
+        public float getPeso()
+        {
+            return this.peso;
+        }
+        public void setPeso(float peso)
+        {
+            this.peso = peso;
+        }
+
+    }
+}
--- AtividadesSemanais/Aula 7/obj/Debug/net5.0/Aula 7.AssemblyInfo.cs
+++ AtividadesSemanais/Aula 7/obj/Debug/net5.0/Aula 7.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Aula 07")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Aula 7")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Aula 07")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Aula 07")]
+[assembly: System.Reflection.AssemblyProductAttribute("Aula 7")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Aula 7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
