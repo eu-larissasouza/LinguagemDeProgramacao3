94a4e507914db6786bbfcde7083918513305676e
Larissa Souza <larissa2004.souza@gmail.com>
2021-08-07 20:44:28 -0300

    Atividade - Aula 8

---
+++ AtividadesSemanais/Aula 8/Casa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_8
+{
+    public class Casa
+    {
+        public float Area { get; set; }
+        public float Altura { get; set; }
+        public decimal Preco { get; set; }
+
+    }
+}
---
+++ AtividadesSemanais/Aula 8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula_8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Casa c = new Casa();
+
+            c.Area = 150;
+            c.Altura = 6.5F;
+            c.Preco = 625M;
+
+            Console.WriteLine("Sua Casa");
+
+            Console.WriteLine("Área: " + c.Area + " m² construídos");
+
+            Console.WriteLine("Altura: " + c.Altura + " metros");
+
+            Console.WriteLine("Preço: R$ " + c.Preco + " mil");
+        }
+    }
+}
